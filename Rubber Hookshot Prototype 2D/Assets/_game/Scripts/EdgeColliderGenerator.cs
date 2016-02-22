e764a65f820829260ca9172caac91f5c7468c818
Ariel Santibanez <ariel.santibanez@futuregame.se>
2016-02-22 17:16:15 +0100

    Multicurve function and spawning of edge colliders.

--- Rubber Hookshot Prototype 2D/Assets/_game/Scripts/EdgeColliderGenerator.cs
+++ Rubber Hookshot Prototype 2D/Assets/_game/Scripts/EdgeColliderGenerator.cs
@@ -5,8 +5,11 @@ using UnityEditor;
 public class EdgeColliderGenerator : MonoBehaviour {
 
     public EdgeCollider2D edge;
-    public EdgeCollider2D edge2;
-    public Vector2[] test;
+   //public EdgeCollider2D edge2;
+	public GameObject edgePrefab;
+
+	public Vector2[] test;
+	public Vector2[] multiTest;
     Vector2 myStart = new Vector2(0f, 0f);
     Vector2 myCp = new Vector2(5f, 5f);
     Vector2 myEnd = new Vector2(10f, 0f);
@@ -18,39 +21,54 @@ public class EdgeColliderGenerator : MonoBehaviour {
 
     void Start ()
     {
-        print(test.Length);
+		/*print(test.Length);
         test = GeneratePoints( myStart, myCp, myEnd, test );
         edge.points = test;
 
         test = GeneratePoints( myStart2, myCp2, myEnd2, test );
-        edge2.points = test;
-    }
+        edge2.points = test;*/
+
+		//multiTest = edge.points;
+		//edge.points = MultiCurve(edge.points);
+		//edge.points = GeneratePoints(myStart2, myCp2, myEnd2, test);
+
+
+
+	}
 
 	void Update ()
     {
         if ( Input.GetKeyDown(KeyCode.P) )
         {
-            
-        }
+			MultiCurve(multiTest);
+
+		}
 	
 	}
 
-    Vector2[] GeneratePoints ( Vector2 start, Vector2 cp, Vector2 end, Vector2[] points )
+	void SpawnCol ( Vector2[] points )
+	{
+		GameObject spawnedCol = (GameObject)Instantiate( edgePrefab, Vector2.zero, Quaternion.identity );
+		spawnedCol.GetComponent<EdgeCollider2D>().points = points;
+		
+	}
+
+    Vector2[] GeneratePoints ( Vector2 start, Vector2 cp, Vector2 end, int amountOfPoints )
     {
-        Vector2[] myArray = new Vector2[points.Length];
+        Vector2[] myArray = new Vector2[amountOfPoints];
 
         float t = 0f;
 
-        for (int i = 0; i < points.Length -1 ; i++)
+        for (int i = 0; i < myArray.Length -1 ; i++)
         {
-            myArray[i] = quadraticBezier(start, cp, end, t);
-            t += (1f / points.Length);
+            myArray[i] = QuadraticBezier(start, cp, end, t);
+            t += (1f / myArray.Length);
         }
         myArray[myArray.Length - 1] = end;
-        return myArray;
+		return myArray;
     }
 
-    Vector2 quadraticBezier (Vector2 start, Vector2 cp, Vector2 end, float t)
+    Vector2 QuadraticBezier (Vector2 start, Vector2 cp, Vector2 end, float t)
     {
         Vector2 pFinal = Vector2.zero;
 
@@ -64,24 +82,41 @@ public class EdgeColliderGenerator : MonoBehaviour {
         return pFinal;
     }
 
-    void GenerateCurve ( Vector2[]points )
+   void MultiCurve ( Vector2[]points )
     {
-        Vector2 p0;
-        Vector2 p1;
-        float midX;
-        float midY;
+		Vector2[] myArray = new Vector2[points.Length];
+	
+		Vector2 start = Vector2.zero;
+		Vector2 cp = Vector2.zero;
+		Vector2 end = Vector2.zero;
 
-        for (int i = 0; i < points.Length - 2; i++)
+		for (int i = 0; i < points.Length - 2; i++)
         {
-            p0 = points[i];
-            p1 = points[i + 1];
-            midX = ( p0.x + p1.x ) / 2;
-            midY = (p0.y + p1.y) / 2;
+			if ( i == 0 )
+			{
+				start = points[i];
+				cp = points[i + 1];
+				end = (cp + points[i + 2]) / 2;
 
-            //Handles.DrawBezier(p0, p1);
-        }
+				SpawnCol( GeneratePoints( start, cp, end, points.Length ) );
 
-        p0 = points[points.Length - 2];
-        p1 = points[points.Length - 1];
-    }
+
+			}
+			else if ( i != 1 )
+			{
+				start = end;
+				cp = points[i];
+				end = ( points[i] + points[i + 1] ) / 2;
+
+				SpawnCol( GeneratePoints( start, cp, end, points.Length ) );
+			} 
+		}
+
+		start = end;
+        cp = points[points.Length - 2];
+		end = points[points.Length - 1];
+
+		SpawnCol( GeneratePoints( start, cp, end, points.Length ) );
+
+	}
 }
