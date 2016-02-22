53b6991c38f877ebe9e1c9d91fd58823e9092879
Ariel Santibanez <ariel.santibanez@futuregames.se>
2016-02-22 23:14:59 +0100

    Continued work on curved 2D collision generation. Renamed a prefab.

--- Rubber Hookshot Prototype 2D/Assets/_game/Scripts/EdgeColliderGenerator.cs
+++ Rubber Hookshot Prototype 2D/Assets/_game/Scripts/EdgeColliderGenerator.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditor;
+using System.Collections.Generic;
 
 public class EdgeColliderGenerator : MonoBehaviour {
 
-    public EdgeCollider2D edge;
+   //public EdgeCollider2D edge;
    //public EdgeCollider2D edge2;
 	public GameObject edgePrefab;
 
 	public Vector2[] test;
-	public Vector2[] multiTest;
+	Vector2[] multiTest = new Vector2[6];
     Vector2 myStart = new Vector2(0f, 0f);
     Vector2 myCp = new Vector2(5f, 5f);
     Vector2 myEnd = new Vector2(10f, 0f);
@@ -19,32 +19,56 @@ public class EdgeColliderGenerator : MonoBehaviour {
     Vector2 myEnd2 = new Vector2(20f, 0f);
 
 
-    void Start ()
+    void Awake ()
     {
-		/*print(test.Length);
-        test = GeneratePoints( myStart, myCp, myEnd, test );
-        edge.points = test;
+        RandomCol( Vector2.zero, 300f, multiTest );
+     
+    }
 
-        test = GeneratePoints( myStart2, myCp2, myEnd2, test );
-        edge2.points = test;*/
-
-		//multiTest = edge.points;
-		//edge.points = MultiCurve(edge.points);
-		//edge.points = GeneratePoints(myStart2, myCp2, myEnd2, test);
-
-
-
-	}
-
-	void Update ()
+   /* void OnDrawGizmos ()
     {
-        if ( Input.GetKeyDown(KeyCode.P) )
+        for (int i = 0; i < multiTest.Length; i++)
         {
-			MultiCurve(multiTest);
+            Gizmos.DrawSphere(multiTest[i], 0.2f);
+        }
+    }*/
 
-		}
+    void Update ()
+    {
+
 	
 	}
+
+    void RandomCol ( Vector2 start, float width, Vector2[]points )
+    {
+        Vector2 prevPoint = Vector2.zero;
+        float increase = width / ( points.Length - 1 );
+        float yDif = width / 10f;
+        float yDifNeg = -yDif;
+        print(yDif);
+        print(yDifNeg);
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            if ( i == 0 )
+            {
+                points[i] = start;
+                prevPoint = points[i];           
+            }
+            else
+            {
+                float yRandom;
+                if (prevPoint.y > 0f)
+                    yRandom = Random.Range( yDifNeg, ( yDifNeg * 2 ) );
+                else
+                    yRandom = Random.Range( yDif, yDif * 2 );
+
+                points[i] = new Vector2( prevPoint.x + increase, prevPoint.y + yRandom );
+                prevPoint = points[i];                
+            }                        
+        }
+        MultiCurve( points );
+    }
 
 	void SpawnCol ( Vector2[] points )
 	{
@@ -52,6 +76,13 @@ public class EdgeColliderGenerator : MonoBehaviour {
 		spawnedCol.GetComponent<EdgeCollider2D>().points = points;
 		
 	}
+
+    void SpawnCol( List<Vector2> points )
+    {
+        GameObject spawnedCol = (GameObject)Instantiate(edgePrefab, Vector2.zero, Quaternion.identity);
+        spawnedCol.GetComponent<EdgeCollider2D>().points = points.ToArray();
+
+    }
 
     Vector2[] GeneratePoints ( Vector2 start, Vector2 cp, Vector2 end, int amountOfPoints )
     {
@@ -82,15 +113,14 @@ public class EdgeColliderGenerator : MonoBehaviour {
         return pFinal;
     }
 
-   void MultiCurve ( Vector2[]points )
+   void MultiCurve ( Vector2[] points )
     {
-		Vector2[] myArray = new Vector2[points.Length];
-	
-		Vector2 start = Vector2.zero;
+        Vector2 start = Vector2.zero;
 		Vector2 cp = Vector2.zero;
 		Vector2 end = Vector2.zero;
+        List<Vector2> vertices = new List<Vector2>();
 
-		for (int i = 0; i < points.Length - 2; i++)
+        for (int i = 0; i < points.Length - 2; i++)
         {
 			if ( i == 0 )
 			{
@@ -98,25 +128,29 @@ public class EdgeColliderGenerator : MonoBehaviour {
 				cp = points[i + 1];
 				end = (cp + points[i + 2]) / 2;
 
-				SpawnCol( GeneratePoints( start, cp, end, points.Length ) );
+				//SpawnCol( GeneratePoints( start, cp, end, points.Length ) ); // Used for separate objects.
+                vertices.AddRange(GeneratePoints(start, cp, end, points.Length));
 
-
-			}
+            }
 			else if ( i != 1 )
 			{
 				start = end;
 				cp = points[i];
 				end = ( points[i] + points[i + 1] ) / 2;
 
-				SpawnCol( GeneratePoints( start, cp, end, points.Length ) );
-			} 
+                //SpawnCol( GeneratePoints( start, cp, end, points.Length ) ); // Used for separate objects.
+                vertices.AddRange(GeneratePoints(start, cp, end, points.Length));
+
+            } 
 		}
 
 		start = end;
         cp = points[points.Length - 2];
 		end = points[points.Length - 1];
 
-		SpawnCol( GeneratePoints( start, cp, end, points.Length ) );
+        //SpawnCol( GeneratePoints( start, cp, end, points.Length ) ); // Used for separate objects.
+        vertices.AddRange(GeneratePoints(start, cp, end, points.Length));
 
-	}
+        SpawnCol( vertices ) ;
+    }
 }
